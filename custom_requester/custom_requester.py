3165e87838ba0eeca3a0cc9cbdfa43a8d35d9ecb
Kostya Egorov <kosta.work0@gmail.com>
2025-05-28 15:20:07 +0300

    add doc string

--- custom_requester/custom_requester.py
+++ custom_requester/custom_requester.py
@@ -13,6 +13,15 @@ class CustomRequester:
 
 
     def send_request(self, method, endpoint, data=None, expected_status=HTTPStatus.OK, need_logging=True):
+        """
+        Враппер для запросов. Позволяет прикручивать различную логику
+        :param method: Метод запроса
+        :param endpoint: Эндпоинт для склейки с BASE_URL в переменной 'url'
+        :param data: Тело запроса. По умолчанию пустое
+        :param expected_status: Ожидаемый статус ответа. Если ожидается иной от SC_OK - передать в методе api-класса
+        :param need_logging: Передача флага для логгирования. По умолчанию = True
+        :return: Возвращает объект ответа
+        """
         url = f"{self.base_url}{endpoint}"
         response = self.session.request(method, url, json=data)
         if need_logging:
@@ -29,6 +38,11 @@ class CustomRequester:
 
 
     def log_req_and_resp(self, response):
+        """
+        Логирование запросов и ответов. Настройки логирования описаны в pytest.ini
+        Преобразует вывод в curl-like (-H хэдеры), (-d тело)
+        :param response: Объект response получаемый из метода "send_request"
+        """
         try:
             request = response.request
             GREEN = '\033[32m'
