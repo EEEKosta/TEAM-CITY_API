fffd7946cc1bcf1af12d89f0d3ef1a0a21381a8f
Kostya Egorov <kosta.work0@gmail.com>
2025-05-30 14:37:09 +0300

    update

--- tests/backend/create_project.py
+++ tests/backend/create_project.py
@@ -1,4 +1,5 @@
 from data.project_data import ProjectData
+from enums.roles import Roles
 
 
 class TestProjectCreate:
@@ -31,3 +32,10 @@ class TestProjectCreate:
             f"expected created project id={self.create_project_id} in project_ids, but not matched"
 
         super_admin.api_object.project_api.clean_up_project(self.create_project_id)
+
+
+    def test_project_create_user(self, super_admin, user_create):
+        project_user = user_create(Roles.PROJECT_ADMIN.value)
+        project_user.api_object.auth_api.auth_and_get_csfr(project_user.creds)
+        created_project = project_user.api_object.project_api.create_project(self.project_data).json()
+        assert created_project['id'] == self.create_project_id
