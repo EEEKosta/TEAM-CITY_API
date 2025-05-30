0804dfffbeab8468a13ce0ee1dc3888ff62afd4e
Kostya Egorov <kosta.work0@gmail.com>
2025-05-30 16:56:48 +0300

    add pydantic model

--- api/project_api.py
+++ api/project_api.py
@@ -7,14 +7,14 @@ class ProjectAPI(CustomRequester):
     def create_project(self, project_data, expected_status=HTTPStatus.OK):
         return self.send_request('POST', '/app/rest/projects', data=project_data, expected_status=expected_status)
 
-
     def get_project(self):
         return self.send_request('GET', '/app/rest/projects')
 
+    def get_project_by_locator(self, locator):
+        return self.send_request('GET', f'/app/rest/projects/{locator}')
 
     def delete_project(self, project_id, expected_status=HTTPStatus.NO_CONTENT):
         return self.send_request('DELETE', f'/app/rest/projects/{project_id}', expected_status=expected_status)
-
 
     def clean_up_project(self, created_project_id):
         self.delete_project(created_project_id)
